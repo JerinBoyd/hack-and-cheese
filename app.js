a6d6573f1bb66f2d88767360de3aec73cd827789
JerinBoyd <maxers@mac.com>
2018-01-30 20:45:27 -0500

    implemented random name generations as well as updaying the page

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+// SOP create variables at top
 const button = document.querySelector('#generateBtn');
 const firstNameEl = document.querySelector('#first');
 const lastNameEl = document.querySelector('#last');
-
+const lastNames = [
+    'cheddar',
+    'gorgonzola',
+    'havarti',
+    'Face',
+];
+    
+const firstNames = [
+    'neo',
+    'morpheus',
+    'trinity',
+    'Jerk',
+];
+// add your event listeners 
 button.addEventListener('click', generateName);
 
+
+//add functions
 function generateName(e){
-    console.log(firstNameEl);
-    console.log(lastNameEl);
+    const rndLastName = randomChoice(lastNames);
+    const rndFirstName = randomChoice(firstNames);
+    firstNameEl.textContent = rndFirstName;
+    lastNameEl.textContent = rndLastName;
+}
+
+function randomChoice(array) {
+    const rndIndex = Math.floor(Math.random() * array.length);
+    return array[rndIndex];
 }
